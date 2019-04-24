1344e79c0f43982c3e432219810917948736241e
backstab319 <dev.siddhartha.gupta@gmail.com>
2019-04-24 21:21:21 +0530

    Update adminpage.php

--- adminpage.php
+++ adminpage.php
@@ -29,6 +29,9 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="/index.php">Home</a>
                             </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/adminpagestore.php">Manage Store</a>
+                            </li>
                         </ul>
                     </div>
                 </div>
@@ -38,7 +41,7 @@
 
     <div class="container jumbotron text-center col-lg-6 col-xl-6">
         <h1 class="display-4">Online Grocery Shopping Cpanel</h1>
-        <p class="lead text-justify text-center">Please use the tools given below to manage the grocery store</p>
+        <p class="lead text-justify text-center">Please use the tools given below to manage the grocery stores users orders</p>
     </div>
 
     <div class="container text-center col-lg-6 col-xl-6">
@@ -110,144 +113,7 @@
         }
     ?>
 
-    <div class="container text-center col-lg-6 col-xl-6" id="add">
-    <h1 class="display-4">Add new item to the store</h1>
-        <div class="form-group d-flex justify-content-center">
-            <form action="adminpage.php" method="POST">
-                <label for="item_grp">Select the item group to update</label>
-                <select name="item_grp" id="item_grp" class="form-control mb-2">
-                    <option value="baked_foods">Baked Foods</option>
-                    <option value="dairy">Dairy</option>
-                    <option value="fast_foods">Fast Foods</option>
-                    <option value="fresh_produce">Fresh Produce</option>
-                    <option value="frozen_foods">Frozen Foods</option>
-                    <option value="household">Household</option>
-                    <option value="meat">Meat</option>
-                    <option value="staples">Staples</option>
-                </select>
-                <input type="text" name="name" placeholder="Enter Product Name" class="form-control mb-2">
-                <input type="number" name="quantity" placeholder="Enter Product Quantity" class="form-control mb-2">
-                <input type="number" name="price" placeholder="Enter Product Price" class="form-control mb-2">
-                <input type="text" name="image" placeholder="Enter Product Image Link" class="form-control mb-2">
-                <input type="submit" name="add" value="Add Item" class="btn btn-outline-primary">
-            </form>
-        </div>
-        <?php
-            if(isset($_POST["add"])){
-                additem();
-            }
-            function additem(){
-                global $conn;
-                $table = $_POST["item_grp"];
-                $name = $_POST["name"];
-                $quantity = $_POST["quantity"];
-                $price = $_POST["price"];
-                $image = $_POST["image"];
-                if(($table and $name and $quantity and $price and $image) != NULL){
-                    $sql = "INSERT INTO $table VALUES('$name','$quantity','$price','$image')";
-                    $conn->query($sql);
-                    echo "<p class='lead text-center text-justify'>Item successfully Added!</p>";
-                }else{
-                    echo "<p class='lead text-center text-justify'>Please check all the fields</p>";
-                }
-            }
-        ?>
-    </div>
-
-    <div class="container text-center col-lg-6 col-xl-6">
-        <h1 class="display-4">Update Store's Information</h1>
-        <div class="form-group d-flex justify-content-center">
-        <form action="adminpage.php" method="POST">
-            <label for="item_grp">Select the item group to update</label>
-            <select name="item_grp" id="item_grp" class="form-control mb-2">
-                <option value="baked_foods">Baked Foods</option>
-                <option value="dairy">Dairy</option>
-                <option value="fast_foods">Fast Foods</option>
-                <option value="fresh_produce">Fresh Produce</option>
-                <option value="frozen_foods">Frozen Foods</option>
-                <option value="household">Household</option>
-                <option value="meat">Meat</option>
-                <option value="staples">Staples</option>
-            </select>
-            <label for="cattrib">Select current the item attribute to change</label>
-            <select name="cattrib" id="cattrib" class="form-control mb-2">
-                <option value="pr_name">Product Name</option>
-                <option value="pr_quantity">Product Quantity</option>
-                <option value="pr_price">Product Price</option>
-                <option value="pr_image">Product Image Link</option>
-            </select>
-            <input type="text" class="form-control mb-2" placeholder="Current Value" name="cval">
-            <label for="attrib">Select the item attribute to change</label>
-            <select name="attrib" id="attrib" class="form-control mb-2">
-                <option value="pr_name">Product Name</option>
-                <option value="pr_quantity">Product Quantity</option>
-                <option value="pr_price">Product Price</option>
-                <option value="pr_image">Product Image Link</option>
-            </select>
-            <input type="text" class="form-control mb-2" placeholder="New Value" name="nval">
-            <input type="submit" value="Update" name="update" class="btn btn-outline-primary form-control mb-2">
-        </form>
-        </div>
-        </div>
-        <?php
-            if(isset($_POST["update"])){
-                update();
-            }
-            function update(){
-                global $conn;
-                $table = $_POST["item_grp"];
-                $col = $_POST["attrib"];
-                $cval = $_POST["cval"];
-                $nval = $_POST["nval"];
-                $ccol = $_POST["cattrib"];
-                if(($table and $col and $cval and $nval and $ccol) != NULL){
-                    $sql = "UPDATE $table SET $col='$nval' WHERE $ccol='$cval'";
-                    $conn->query($sql);
-                    echo "<p class='lead text-center text-justify'>Item successfully updated!</p>";
-                }else{
-                    echo "<p class='lead text-center text-justify'>Please check all the input fields</p>";
-                }
-            }
-        ?>
-    </div>
-
-    <div class="container text-center col-lg-6 col-xl-6">
-        <h1 class="display-4">Delete Store items</h1>
-        <div class="form-group d-flex justify-content-center">
-            <form action="adminpage.php" method="POST">
-                <label for="item_grp">Select the item group to update</label>
-                <select name="item_grp" id="item_grp" class="form-control mb-2">
-                    <option value="baked_foods">Baked Foods</option>
-                    <option value="dairy">Dairy</option>
-                    <option value="fast_foods">Fast Foods</option>
-                    <option value="fresh_produce">Fresh Produce</option>
-                    <option value="frozen_foods">Frozen Foods</option>
-                    <option value="household">Household</option>
-                    <option value="meat">Meat</option>
-                    <option value="staples">Staples</option>
-                </select>
-                <input type="text" name="name" placeholder="Product name" class="form-control mb-2">
-                <input type="submit" name="delete_item" value="Delete Item" class="btn btn-primary">
-            </form>
-        </div>
-        <?php
-            if(isset($_POST["delete_item"])){
-                delete_item();
-            }
-            function delete_item(){
-                global $conn;
-                $table = $_POST["item_grp"];
-                $name = $_POST["name"];
-                if(($table and $name)!= NULL){
-                    $sql = "DELETE FROM $table WHERE pr_name='$name'";
-                    $conn->query($sql);
-                    echo "<p class='lead text-center text-justify'>Item successfully Deleted!</p>";
-                }else{
-                    echo "<p class='lead text-center text-justify'>Item Not Deleted. Please check your inputs.</p>";
-                }
-            }
-        ?>
-    </div>
+    
 
     <div class="container text-center col-lg-6 col-xl-6">
         <h1 class="display-4">Delete completed orders</h1>
